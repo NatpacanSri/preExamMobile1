2e5967dee5d5007d1f44f64ea9b243fbd572446e
NatpacanSribanhad <natpacan.s@kkumail.com>
2023-01-26 16:16:05 +0700

    Update SecondActivity.kt

--- app/src/main/java/com/example/preexammobile/SecondActivity.kt
+++ app/src/main/java/com/example/preexammobile/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.preexammobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.preexammobile1.R
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
